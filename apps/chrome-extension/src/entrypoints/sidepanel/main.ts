4ff8120d38d1cd0294b60f818877aeb1cbf1b111
Peter Steinberger <steipete@gmail.com>
2026-01-12 07:25:13 +0000

    fix: fix sidepanel message switch

--- apps/chrome-extension/src/entrypoints/sidepanel/main.ts
+++ apps/chrome-extension/src/entrypoints/sidepanel/main.ts
@@ -2047,12 +2047,6 @@ function handleBgMessage(msg: BgToPanel) {
       panelState.lastMeta = { inputSummary: null, model: null, modelLabel: null }
       void streamController.start(msg.run)
       return
-    case 'chat:history':
-      handleChatHistoryResponse(msg)
-      return
-    case 'agent:chunk':
-      handleAgentChunk(msg)
-      return
     }
     case 'chat:history':
       handleChatHistoryResponse(msg)
